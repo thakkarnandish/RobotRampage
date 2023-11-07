5115f4894cbf7590c4c82cd68f4db509b5739e47
thakkarnandish <Nandish2901>
2023-11-07 15:53:14 -0500

    Chapter 9 - Addeed Guns/Weapons

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -9,7 +9,7 @@ public class Gun : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastFireTime = Time.time - 10;
     }
 
     // Update is called once per frame
