fbc6e5e24ed07abdac1aefcaa0b0f4b1f58dbb9b
thakkarnandish <Nandish2901>
2023-11-14 13:59:58 -0500

    Chapter 10 - Added Sounds and Concluded

--- Assets/Scripts/RobotSpawn.cs
+++ Assets/Scripts/RobotSpawn.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class RobotSpawn : MonoBehaviour
 {
+    [SerializeField]
+    GameObject[] robots;
+    private int timesSpawned;
+    private int healthBonus = 0;
+
+    public void SpawnRobot()
+    {
+        timesSpawned++;
+        healthBonus += 1 * timesSpawned;
+        GameObject robot = Instantiate(robots[Random.Range(0, robots.Length)]);
+        robot.transform.position = transform.position;
+        robot.GetComponent<Robot>().health += healthBonus;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
